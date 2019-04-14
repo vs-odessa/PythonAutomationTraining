57bcf24e32a8313344029979e8481a423d6bb263
Vladimir <vs-odessa@yandex.ru>
2019-04-14 14:00:32 +0300

    fibonacci + test

---
+++ tests/test_fibonacci.py
@@ -0,0 +1,11 @@
+import pytest
+from functions.fibonacci import generate_fibonacci
+
+my_list = generate_fibonacci(5)
+print(*my_list)
+
+class Tests():
+    def test_case1_positive(self):
+        actual_result = generate_fibonacci(5)
+        expected_result = [1, 1, 2, 3, 5]
+        assert (actual_result == expected_result)
