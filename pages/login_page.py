b67f041e82f014d426a84a3c535188290bc6aff9
Vladimir <vs-odessa@yandex.ru>
2019-05-19 13:25:40 +0300

    Adding first selenium test

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+    LOGIN_INPUT = (By.ID, "login-form-username")
+    PASSWORD_INPUT = (By.ID, "login-form-password")
+    LOGIN_BUTTON = (By.ID, "login")
+    driver = None
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def login_to_jira(self):
+        self.driver.find_element(*self.LOGIN_INPUT).clear()
+        self.driver.find_element(*self.LOGIN_INPUT).send_keys("VolodymyrStepanov")
+        self.driver.find_element(*self.PASSWORD_INPUT).clear()
+        self.driver.find_element(*self.PASSWORD_INPUT).send_keys("VolodymyrStepanov")
+        self.driver.find_element(*self.LOGIN_BUTTON).submit()
+
+    def at_page(self):
+        return "System Dashboard - Hillel IT School JIRA" in self.driver.title
+
+    def open(self):
+        self.driver.get("http://jira.hillel.it/secure/Dashboard.jspa")
+        return self
