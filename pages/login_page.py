a391917982b9b516d726a994dc5f2bd8b848b374
Vladimir <vs-odessa@yandex.ru>
2019-05-19 13:31:26 +0300

    Cleaning & refinement test

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class LoginPage:
     LOGIN_INPUT = (By.ID, "login-form-username")
     PASSWORD_INPUT = (By.ID, "login-form-password")
     LOGIN_BUTTON = (By.ID, "login")
+    MY_NAME = "VolodymyrStepanov"
+    MY_PASS = "VolodymyrStepanov"
+    TEST_URL = "http://jira.hillel.it/secure/Dashboard.jspa"
     driver = None
 
     def __init__(self, driver):
@@ -12,14 +16,18 @@ class LoginPage:
 
     def login_to_jira(self):
         self.driver.find_element(*self.LOGIN_INPUT).clear()
-        self.driver.find_element(*self.LOGIN_INPUT).send_keys("VolodymyrStepanov")
+        self.driver.find_element(*self.LOGIN_INPUT).send_keys(self.MY_NAME)
         self.driver.find_element(*self.PASSWORD_INPUT).clear()
-        self.driver.find_element(*self.PASSWORD_INPUT).send_keys("VolodymyrStepanov")
+        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(self.MY_PASS)
         self.driver.find_element(*self.LOGIN_BUTTON).submit()
 
     def at_page(self):
         return "System Dashboard - Hillel IT School JIRA" in self.driver.title
 
+    def login_form_presented(self):
+        return WebDriverWait(self.driver, 10)\
+            .until(ec.presence_of_element_located((By.ID, "login-form-username")))
+
     def open(self):
-        self.driver.get("http://jira.hillel.it/secure/Dashboard.jspa")
+        self.driver.get(self.TEST_URL)
         return self
--- selenium_tests/login_tests.py
+++ selenium_tests/login_tests.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 from pages.login_page import LoginPage
@@ -16,24 +13,10 @@ class TestJira:
         self.main_page = MainPage(self.driver)
 
     def test_login_to_jira(self):
-        # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-        # login_page = LoginPage(driver)
         self.login_page.open()
-        # driver.get("https://jira.hillel.it/secure/Dashboard.jspa")
-        # assert "System Dashboard - Hillel IT School JIRA" in driver.title
         assert self.login_page.at_page()
-
-        # profile_logo_element = WebDriverWait(driver, 10)\
-        #     .until(ec.presence_of_element_located((By.ID, "header-details-user-fullname")))
-
-        login_form_element = WebDriverWait(self.driver, 10)\
-            .until(ec.presence_of_element_located((By.ID, "login-form-username")))
-
-        # assert profile_logo_element.is_displayed(), "Profile logo element was not found"
-        assert login_form_element.is_displayed()
-
+        assert self.login_page.login_form_presented()
         self.login_page.login_to_jira()
-
         assert self.main_page.logged_in()
 
     def teardown_method(self):
