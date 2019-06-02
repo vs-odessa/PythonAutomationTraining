5544daf8718280ca7277cadfbd5c21b958a81b1c
Vladimir <vs-odessa@yandex.ru>
2019-06-02 19:20:49 +0300

    rename ui tests folder and adding test screeshot

---
+++ functions/test_helper.py
@@ -0,0 +1,13 @@
+import allure
+from functions.data_helper import get_current_timestamp_string
+
+
+def attach_screenshot(driver, name=""):
+    if name == "":
+        name = "screenshot" + get_current_timestamp_string()
+    try:
+        allure.attach(driver.get_screenshot_as_png(),
+                      name=name,
+                      attachment_type=allure.attachment_type.PNG)
+    except Exception as e:
+        print(e)
--- tests_ui/test_jira_login.py
+++ tests_ui/test_jira_login.py
@@ -1,7 +1,10 @@
+import sys
+sys.path.append('.')
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
+from functions.test_helper import attach_screenshot
 
 
 class TestLoginJira:
@@ -23,6 +26,7 @@ class TestLoginJira:
         assert self.login_page.at_page()
         assert self.login_page.login_form_presented()
         self.login_page.login_to_jira("wrongusername", self.login_page.MY_PASS)
+        attach_screenshot(self.driver)
         assert self.login_page.auth_failed()
 
     def test_login_to_jira_wrong_password(self):
