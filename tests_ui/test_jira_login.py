8a4767bb03f64a7deb9f34b7fcaa0e641aae649b
Vladimir <vs-odessa@yandex.ru>
2019-06-02 19:30:58 +0300

    Few test & ci updates

--- tests_ui/test_jira_login.py
+++ tests_ui/test_jira_login.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from functions.test_helper import attach_screenshot
+import allure
 
 
 class TestLoginJira:
@@ -14,6 +15,8 @@ class TestLoginJira:
         self.login_page = LoginPage(self.driver)
         self.main_page = MainPage(self.driver)
 
+    @allure.tag('ui')
+    @allure.title('Login to Jira - positive')
     def test_login_to_jira_positive(self):
         self.login_page.open()
         assert self.login_page.at_page()
@@ -21,6 +24,17 @@ class TestLoginJira:
         self.login_page.login_to_jira(self.login_page.MY_NAME, self.login_page.MY_PASS)
         assert self.main_page.logged_in()
 
+    @allure.tag('ui')
+    @allure.title('Login to Jira - wrong username')
+    def test_login_to_jira_wrong_username(self):
+        self.login_page.open()
+        assert self.login_page.at_page()
+        assert self.login_page.login_form_presented()
+        self.login_page.login_to_jira("wrongusername", self.login_page.MY_PASS)
+        assert self.login_page.auth_failed()
+
+    @allure.tag('ui')
+    @allure.title('Login to Jira - wrong username + Screenshot')
     def test_login_to_jira_wrong_username(self):
         self.login_page.open()
         assert self.login_page.at_page()
@@ -29,11 +43,14 @@ class TestLoginJira:
         attach_screenshot(self.driver)
         assert self.login_page.auth_failed()
 
+    @allure.tag('ui')
+    @allure.title('Login to Jira - wrong password + Screenshot')
     def test_login_to_jira_wrong_password(self):
         self.login_page.open()
         assert self.login_page.at_page()
         assert self.login_page.login_form_presented()
         self.login_page.login_to_jira(self.login_page.MY_NAME, "wrongpassword")
+        attach_screenshot(self.driver)
         assert self.login_page.auth_failed()
 
     def teardown_method(self):
